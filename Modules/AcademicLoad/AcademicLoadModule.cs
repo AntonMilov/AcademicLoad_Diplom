e76536afe44564d5922bc4061f66c3bc0b202d77
AntonMilov <imfm37587@gmail.com>
2022-02-07 16:55:36 +0300

    create Architecture solution

---
+++ Modules/AcademicLoad/AcademicLoadModule.cs
@@ -0,0 +1,20 @@
+﻿using AcademicLoad.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace AcademicLoad
+{
+    public class AcademicLoadModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
