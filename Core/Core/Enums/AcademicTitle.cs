29654d482b60271e3efbf431f1c81b8cb9365c16
AntonMilov <imfm37587@gmail.com>
2022-02-07 18:26:28 +0300

    add: add enum AcademicTitle

---
+++ Core/Core/Enums/AcademicTitle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    /// <summary>
+    /// Учёная степень.
+    /// </summary>
+    public enum AcademicTitle
+    {
+        /// <summary>
+        /// Декан
+        /// </summary>
+        Dekan,
+
+        /// <summary>
+        /// Зав. кафедрой.
+        /// </summary>
+        HeadDepartment,
+
+        /// <summary>
+        /// Профессор.
+        /// </summary>
+        Professor,
+
+        /// <summary>
+        /// Доцент.
+        /// </summary>
+        Docent,
+
+        /// <summary>
+        /// Старший преподователь.
+        /// </summary>
+        SeniorLecturer,
+
+        /// <summary>
+        /// Преподователь.
+        /// </summary>
+        Lecturer,
+
+        /// <summary>
+        /// Ассистент.
+        /// </summary>
+        Assistant,
+    }
+}
