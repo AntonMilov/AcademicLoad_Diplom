7decea1aacd85590077f677bd41b4552f250e5f0
AntonMilov <imfm37587@gmail.com>
2022-02-13 18:59:20 +0300

    add big

--- Modules/AcademicLoad/ViewModels/TeachersViewModel.cs
+++ Modules/AcademicLoad/ViewModels/TeachersViewModel.cs
@@ -4,16 +4,9 @@ namespace AcademicLoadModule.ViewModels
 {
     public class TeachersViewModel : BindableBase
     {
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
-
         public TeachersViewModel()
         {
-            Message = "View A from your Prism Module";
+         
         }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace Shell.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = Properties.Resources.Title;
         public string Title
         {
             get { return _title; }
