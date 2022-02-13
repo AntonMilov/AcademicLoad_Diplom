e503b344c83281c6ba1ff0334939f213043c6cab
AntonMilov <imfm37587@gmail.com>
2022-02-13 17:05:26 +0300

    add: add Group

---
+++ Data/Data/Models/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    /// <summary>
+    /// Модель группы.
+    /// </summary>
+    public class Group
+    {
+        /// <summary>
+        /// Название группы.
+        /// </summary>
+        public string Name {get; set;}
+
+        /// <summary>
+        /// Кол-во студентов.
+        /// </summary>
+        public int Students { get; set; }
+    }
+}
