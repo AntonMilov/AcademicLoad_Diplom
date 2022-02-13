26cae26e18c87667385a5095cd81e4bc1a4318e8
AntonMilov <imfm37587@gmail.com>
2022-02-13 16:55:34 +0300

    add: add Rate

---
+++ Data/Data/Enums/Rate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Enums
+{
+    /// <summary>
+    /// Ставка преподователя.
+    /// </summary>
+    public enum Rate
+    {
+        /// <summary>
+        /// Почасовая ставка.
+        /// </summary>
+        RateHourly,
+
+        /// <summary>
+        /// Ставка 0,25.
+        /// </summary>
+        Rate25,
+
+        /// <summary>
+        /// Ставка 0,50.
+        /// </summary>
+        Rate50,
+
+        /// <summary>
+        /// Ставка 0,75.
+        /// </summary>
+        Rate75,
+
+        /// <summary>
+        /// Ставка 1,00.
+        /// </summary>
+        Rate100
+    }
+}
