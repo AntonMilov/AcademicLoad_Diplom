1bfe1ba662a7f10b7db1f3b31d5d1b0a47a1561f
AntonMilov <imfm37587@gmail.com>
2022-02-16 19:16:10 +0300

    refactor TeachersView

---
+++ AcademicLoad/Views/TabsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AcademicLoadModule.Views
+{
+    /// <summary>
+    /// Interaction logic for TabsView
+    /// </summary>
+    public partial class TabsView : UserControl
+    {
+        public TabsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
